0286b97c5d15f2998aad15600d3779f5f6a7dd03
itsfeas <amf.fiaz@gmail.com>
2022-08-24 21:03:50 -0600

    completed footer

--- client/src/components/socialsBlock/SocialsBlock.js
+++ client/src/components/socialsBlock/SocialsBlock.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import GradientImg from '../../commons/gradient/GradientImg';
-import githubLogo from "../../socials/github.svg";
-import linkedinLogo from "../../socials/linkedin.svg";
+import { GradientImgUnbordered } from '../../commons/gradient/GradientImg';
+import instagramIcon from "../../socials/instagram.svg";
+import webIcon from "../../socials/web.svg";
 import emailIcon from "../../socials/mail.svg";
 
 import blockStyle from '../Block';
@@ -12,22 +12,22 @@ function SocialsBlock() {
   return (
     <div className="footer-block" id="footer-block">
       <div className="footer">
-          <p className="footer-text">Built with React</p>
+          <p className="footer-text unecessary-footer-text">Built with React</p>
           <p className="divider">|</p>
-          <p className="footer-text highlighted-text footer-link"><a href="https://github.com/itsfeas/personal-website">Website Repo</a></p>
+          <p className="footer-text">Property of Computer Engineering Club</p>
           {/* <p className="footer-text">Property Muhammad Fiaz</p> */}
           <p className="divider">|</p>
 
-          <div className="socials-content" id={"socials-content"}>
-              <a href="https://github.com/itsfeas">
-                <GradientImg img={githubLogo} className="socials-icon highlighted" />
+          <div className="socials-content">
+            <a href="https://www.compeclub.com/">
+              <img src={webIcon} className="socials-icon highlighted" />
               </a>
-              <a href="https://www.linkedin.com/in/amf-fiaz/">
-                <GradientImg img={linkedinLogo} className="socials-icon highlighted" />
-              </a>
-              <a href="mailto:mfiaz@ualberta.ca">
-                <GradientImg img={emailIcon} className="socials-icon highlighted" />
-              </a>
+            <a href="mailto:external@compeclub.com">
+              <img src={emailIcon} className="socials-icon highlighted" />
+            </a>
+            <a href="https://www.instagram.com/compeclub/">
+              <img src={instagramIcon} className="socials-icon highlighted" />
+            </a>
           </div>
         </div>
     </div>
