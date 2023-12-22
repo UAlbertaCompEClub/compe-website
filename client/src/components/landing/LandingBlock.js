cbaea0e5342c28c5d84a6a035d82acfaca9a2b15
rachel-ellis <rjellis@ualberta.ca>
2023-12-22 15:04:39 -0700

    feat: initial changes

--- client/src/components/landing/LandingBlock.js
+++ client/src/components/landing/LandingBlock.js
@@ -1,12 +1,8 @@
 import React, { useEffect } from 'react';
 // import landingStyle from './Block';
 import './LandingBlock.css'
-import landingGraphic from './LandingGraphic.svg';
-import blob1 from './blob/blob.svg';
 import navLinker from "../pageState/observer/navLinker";
-import floater1 from './floaters/line.svg';
-import background from './background/background.svg';
-import GradientImg from '../../commons/gradient/GradientImg';
+import CompELogo from './CompELogo.svg';
 
 var LandingBlock = React.forwardRef((props, ref) => {
     var id = "landing-block";
@@ -19,19 +15,12 @@ var LandingBlock = React.forwardRef((props, ref) => {
                 <div className="landing-overlay">
                     <div className="landing-text text-main" id="landing-text">
                         <div className='landing-logo-container'>
-                            <h1 className='main-heading faded text-main'>Career</h1>
-                            <h1 className='main-heading faded gradient-header'>++</h1>
+                            <h1 className='main-heading faded text-main'>Computer Engineering Club</h1>
                         </div>
-                        <h2 className='sub-heading faded text-main'>Presented by the Computer Engineering Club</h2>
-                        <h3 className='faded colored-text'>January 18th - 19th, 2024</h3>
+                        <h2 className='sub-heading faded text-main'>Promote sense of community<br/>Provide resources and services to facilitate career success<br/>Present opportunities to network and develop technical skills</h2>
                     </div>
                     <div className='landing-img-container faded'>
-                        <GradientImg img={landingGraphic} className="landing-img" alt="Cool Graphic" />
-                    </div>
-                </div>
-                <div className="landing-underlay">
-                    <div className="landing-background">
-                        <img src={background} className="landing-background-img" alt="Cool Graphic" />
+                        <img src={CompELogo} />
                     </div>
                 </div>
             </div>
