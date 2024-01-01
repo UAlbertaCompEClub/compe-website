cbd8c0f687049b4bbf0d5d7e1b26ec25f99dfe5c
rachel-ellis <rjellis@ualberta.ca>
2024-01-01 15:27:17 -0700

    fix: get calendar to work

--- client/src/components/eventBlock/EventBlock.js
+++ client/src/components/eventBlock/EventBlock.js
@@ -4,7 +4,7 @@ import navLinker from "../pageState/observer/navLinker";
 import EventCard from "./EventCard";
 import events from "./events.json";
 import Typography from "@mui/material/Typography";
-import { Paper, Typography } from "@mui/material";
+import { Paper } from "@mui/material";
 
 const id = "events";
 
@@ -28,14 +28,14 @@ const EventBlock = React.forwardRef((props, ref) => {
           </Grid>
         ))}
       </Grid>
-      <Paper elevation={3} style={{ padding: 16 }}>
-        <Typography gutterBottom variant="h5" component="div" marginTop="2rem">
+      <Paper elevation={3} style={{ padding: 16, backgroundColor: "#42654A"}}>
+        <Typography gutterBottom variant="h5" component="div" marginTop="2rem" color={"white"}>
           Check out all our planned events in the calendar below!
         </Typography>
-        <div>
+        <div align="center">
           <iframe
             src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FEdmonton&title=CompE%20Club%202023%2F24%20Events&src=Y185OTBkNDAwODcwZGI1ZWMxYmY3MTI2N2VjYzcyYzMyYzI3NTA3NjZiOTk1ZmFjY2M2ZjczZWI3ZmE4Y2RjZTQ2QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237CB342"
-            style="border:solid 1px #777"
+            style={{border: "solid 1px #777"}}
             width="800"
             height="600"
             frameborder="0"
